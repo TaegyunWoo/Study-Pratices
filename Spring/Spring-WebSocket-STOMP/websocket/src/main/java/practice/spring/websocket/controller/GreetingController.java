8b929f97b89cfbfc6428476948e2d4fc29fbaf46
TaegyunWoo <dnxprbs@gmail.com>
2023-12-10 17:52:05 +0900

    Update Spring STOMP Study

--- Spring/Spring-WebSocket-STOMP/websocket/src/main/java/practice/spring/websocket/controller/GreetingController.java
+++ Spring/Spring-WebSocket-STOMP/websocket/src/main/java/practice/spring/websocket/controller/GreetingController.java
@@ -4,8 +4,11 @@
  */
 package practice.spring.websocket.controller;
 
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 import practice.spring.websocket.dto.GreetingMessage;
@@ -18,5 +21,11 @@ public class GreetingController {
   public GreetingMessage greeting(HelloMessage message) throws Exception {
     Thread.sleep(1000); // simulated delay
     return new GreetingMessage("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+  }
+
+  @SubscribeMapping("/greetings")
+  public void greetingSub(MessageHeaders headers, MessageHeaderAccessor accessor) throws Exception {
+    System.out.println(headers.toString() + "\n");
+    System.out.println(accessor.toString());
   }
 }
