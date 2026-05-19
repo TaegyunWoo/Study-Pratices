797650b01e439a78479ea7003c3a1d75904038a5
Jay(우태균) <tkwoo@gabia.com>
2026-05-19 19:10:48 +0900

    nestjs-from-scratch : runnable api

--- nestjs-guide-study/scratch/src/main.ts
+++ nestjs-guide-study/scratch/src/main.ts
@@ -1,4 +1,5 @@
 import {Controller, Module, Get} from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
 
 @Controller()
 class AppController {
@@ -7,3 +8,21 @@ class AppController {
         return 'Hello World!';
     }
 }
+
+/**
+ * NestJS가 실행될때, Module 데코레이터가 붙은 클래스를 보고, 속성으로 전달된 모든 컨트롤러를 인스턴스로 생성한다.
+ */
+@Module({
+    controllers: [AppController]
+})
+class AppModule {
+
+}
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule); //AppModule을 생성해라.
+
+    await app.listen(3001); // 3001번 포트로 요청을 받는다.
+}
+
+bootstrap();
