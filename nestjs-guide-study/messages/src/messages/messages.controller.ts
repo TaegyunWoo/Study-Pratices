846c495430a151555017ff764650cfe77db7cceb
Jay(우태균) <tkwoo@gabia.com>
2026-05-27 18:35:14 +0900

    nestjs-messages : 에러 응답 기본

--- nestjs-guide-study/messages/src/messages/messages.controller.ts
+++ nestjs-guide-study/messages/src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -43,7 +43,11 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    return this.messagesService.findOne(id);
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) throw new NotFoundException('message not found'); //에러 응답
+
+    return message;
   }
 }
