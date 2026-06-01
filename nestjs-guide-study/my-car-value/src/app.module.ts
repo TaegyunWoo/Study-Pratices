39eb1e0efac7d476c43380e255a06cdadf69dcf1
Jay(우태균) <tkwoo@gabia.com>
2026-06-01 19:31:00 +0900

    nestjs-my-car-value : 기본 엔티티 생성

--- nestjs-guide-study/my-car-value/src/app.module.ts
+++ nestjs-guide-study/my-car-value/src/app.module.ts
@@ -4,14 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'better-sqlite3', //DB 종류
       database: 'db.sqlite', //DB 명
-      entities: [],
-      autoLoadEntities: true,
+      entities: [User, Report], // User, Report 엔티티 설정
       synchronize: true,
     }),
     UsersModule,
--- nestjs-guide-study/my-car-value/src/users/users.module.ts
+++ nestjs-guide-study/my-car-value/src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])], // 해당 설정으로 인해 UserRepository 가 생성된다.
   controllers: [UsersController],
   providers: [UsersService]
 })
