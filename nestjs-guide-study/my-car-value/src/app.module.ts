747ad302e38c654efe3e68da9f7992472928b067
Jay(우태균) <tkwoo@gabia.com>
2026-06-01 19:18:23 +0900

    nestjs-my-car-value : sqlite 및 typeorm 연동

--- nestjs-guide-study/my-car-value/src/app.module.ts
+++ nestjs-guide-study/my-car-value/src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3', //DB 종류
+      database: 'db.sqlite', //DB 명
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
