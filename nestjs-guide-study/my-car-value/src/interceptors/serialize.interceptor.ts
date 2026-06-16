cdba19573e0eeba795f78b32e75c13bf5487b982
Jay(우태균) <tkwoo@gabia.com>
2026-06-16 19:03:14 +0900

    nestjs-my-car-value : 응답 DTO 자동 변환 인터셉터 작성 및 적용

--- nestjs-guide-study/my-car-value/src/interceptors/serialize.interceptor.ts
+++ nestjs-guide-study/my-car-value/src/interceptors/serialize.interceptor.ts
@@ -1,25 +1,22 @@
-import {
-  UseInterceptors,
-  NestInterceptor,
-  ExecutionContext,
-  CallHandler
-} from '@nestjs/common';
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private readonly dto: any) {}
+
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> {
-    console.log('Im running before the handler', context);
-
     return next.handle().pipe(
       map((data: any) => {
         // Run something before the response is sent out
         console.log('Im running before the response is sent out');
-        return data;
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
       }),
     );
   }
